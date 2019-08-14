6d68961fccd788125ad057f7fdc8ae44c810e0a3
empty.dubs <13712788+max-t-wheeler@users.noreply.github.com>
2019-08-14 03:43:52 -0400

    removed references to discogs_client and associated methods

--- server/venv/test-app.py
+++ server/venv/test-app.py
@@ -3,7 +3,6 @@ from flask_cors import CORS
 import time
 
 from config import USER_AGENT, API_TOKEN, API_KEY, API_SECRET
-from discogs_client import Client
 from discogs_client2 import DiscogsClient
 from discograph2 import DiscoGraph
 
@@ -14,36 +13,10 @@ DEBUG = True
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-app.config['CLIENT'] = Client(USER_AGENT, user_token=API_TOKEN)
-app.config['CLIENT2'] = DiscogsClient(USER_AGENT, API_KEY, API_SECRET)
+app.config['CLIENT'] = DiscogsClient(USER_AGENT, API_KEY, API_SECRET)
 
 # enable CORS
 CORS(app, resource={r'/*': {'origins': '*'}})
-
-
-@app.route('/generate_graph', methods=['GET', 'POST'])
-def generate_graph():
-    response_object = {
-            'status': 'success'
-    }
-    if request.method == 'POST':
-        graph_data = request.get_json()
-
-        connection = graph_data['connection'].lower()
-        num_steps = int(graph_data['num_steps'])
-        resource_url = graph_data['resource_url']
-        source_type = graph_data['source_type'].lower()
-        target_type = graph_data['target_type'].lower()
-
-        # construct graph
-        graph = DiscoGraph(app.config['CLIENT'], resource_url, connection, source_type, target_type, 'node_link')
-
-        graph.generate(num_steps)
-        response_object['graph_data'] = graph.export()
-        app.config['GRAPH_DATA'] = response_object['graph_data']
-    else:
-        response_object['graph_data'] = app.config['GRAPH_DATA']
-    return jsonify(response_object)
 
 
 @app.route('/get_graph_data', methods=['GET', 'POST'])
@@ -61,11 +34,11 @@ def get_graph_data():
         target_type = graph_data['target_type'].lower()
 
         # construct graph
-        graph = DiscoGraph(app.config['CLIENT2'], connection, source_id, source_type, target_type, 'node_link')
+        graph = DiscoGraph(app.config['CLIENT'], connection, source_id, source_type, target_type, 'node_link')
         graph.generate(num_steps)
 
         # graph = {
-        #     'nodes': [app.config['CLIENT2'].get_resource(source_id, source_type)],
+        #     'nodes': [app.config['CLIENT'].get_resource(source_id, source_type)],
         #     'links': []
         # }
 
@@ -114,7 +87,7 @@ def get_search_results():
         source_type = query_data['source_type']
         text = query_data['text']
 
-        search_data = app.config['CLIENT2'].search(text, type=source_type)
+        search_data = app.config['CLIENT'].search(text, type=source_type)
         response_object['query_data'] = search_data['results'][0:count]
     else:
         response_object['query_data'] = app.config['QUERY_DATA']
